263b1519bae0804c1188878baf1ce874dd6043db
Eric Quan <equan1090@gmail.com>
2022-01-08 15:05:46 -0700

    can now generate new arrays, also can save in local storage

--- src/components/ArrayBlocks/ArrayBlocks.jsx
+++ src/components/ArrayBlocks/ArrayBlocks.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import './ArrayBlocks.css'
-function ArrayBlocks() {
-    let range;
+function ArrayBlocks({props}) {
 
-    const randomNum = () => {
-        let random = []
-        for(let i = 0; i < 300; i++) {
-            random.push(Math.floor(Math.random() * (900 - 5) + 5))
-        }
-        return random
-    }
-
-    range = randomNum();
-    console.log('this is range', range)
     return(
         <div className='array-wrapper'>
-            {range.map((num, idx) => (
+            {props.map((num, idx) => (
                 <div className='each-box' key={idx} style={{height: `${num}px`}}></div>
             ))}
 
--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import ArrayBlocks from "../ArrayBlocks/ArrayBlocks";
+
 import './NavBar.css'
+
 function NavBar() {
 
+    const [arr, setArr] = useState([])
+    const [arrSize, setArrSize] = useState(5)
+
+    const generateArr = () => {
+        let random = []
+        for(let i = 0; i < arrSize; i++) {
+            random.push(Math.floor(Math.random() * (900 - 5) + 5))
+        }
+        return random
+    }
+
+
+    //Uses localStorage to keep state on refresh
+    useEffect(() => {
+        const data = localStorage.getItem('current-array')
+        if(data) {
+            setArr(JSON.parse(data))
+        }
+    }, [])
+    //Keeps the current array and saves in local storage
+    useEffect(() => {
+        localStorage.setItem('current-array', JSON.stringify(arr))
+    })
+
+
     return(
-        <nav className='top-nav-menu'>
-            <ul>
-                <li>New Array</li>
-                <li>Array Size</li>
-                <li>Sorting Method</li>
-            </ul>
-        </nav>
+        <>
+            <nav className='top-nav-menu'>
+                <ul>
+                    <li onClick={() => setArr(generateArr())}>New Array</li>
+                    <div>
+                        <li>Array Size</li>
+                        <input type="number"
+                            value={arrSize}
+                            min="5"
+                            max="300"
+                            onChange={() => setArrSize()}/>
+                    </div>
+                    <li>Sorting Method</li>
+                </ul>
+            </nav>
+
+            <ArrayBlocks props={arr}/>
+        </>
     )
 
 }
