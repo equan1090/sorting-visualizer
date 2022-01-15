7c9208cecbc2448dcb14c4d54d492fdef47595a4
Eric Quan <equan1090@gmail.com>
2022-01-15 13:13:39 -0700

    added quick sort algo

--- src/SortingAlgs/BubbleSort.js
+++ src/SortingAlgs/BubbleSort.js
@@ -1,15 +1,16 @@
-// function BubbleSort(arr) {
-//     const animation = []
-//     if (arr.length <= 1) return arr;
+export function BubbleSort(arr) {
+    const animation = []
+    if (arr.length <= 1) return arr;
 
-//     for(let i = 0; i < arr.length; i++) {
-//         for(let j = 0; j < arr.length - i - 1; j++) {
-//             animation.push(i, j)
-//             if(arr[j] > arr[j + 1]) {
-//                 arr[j], arr[j+1] = arr[j+1], arr[j]
-//             }
-//         }
-//     }
-// }
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length - i - 1; j++) {
+            animation.push(i, j)
+            if(arr[j] > arr[j + 1]) {
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            }
+        }
+    }
+    return animation
+}
 
 
---
+++ src/SortingAlgs/QuickSort.jsx
@@ -0,0 +1,36 @@
+function QuickSort(arr) {
+    quickSortHelper(arr, 0, len(arr) - 1)
+    return arr
+}
+
+function quickSortHelper(arr, startIdx, endIdx) {
+    if (startIdx >= endIdx) {
+        return
+    }
+    pivotIdx = startIdx
+    leftIdx = startIdx + 1
+    rightIdx = endIdx
+
+    while(rightIdx >= leftIdx) {
+        if(arr[leftIdx] > arr[pivotIdx] && arr[rightIdx] < arr[pivotIdx]) swap(leftIdx, rightIdx, arr)
+
+        if(arr[leftIdx] <= arr[pivotIdx]) leftIdx += 1
+
+        if(arr[rightIdx] >= arr[pivotIdx]) rightIdx -= 1
+    }
+    swap(pivotIdx, rightIdx, arr)
+    leftSubArrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1)
+
+    if(leftSubArrayIsSmaller) {
+        quickSortHelper(arr, startIdx, rightIdx - 1)
+        quickSortHelper(arr, rightIdx + 1, endIdx)
+    } else{
+        quickSortHelper(arr, rightIdx + 1, endIdx)
+        quickSortHelper(arr, startIdx, rightIdx - 1)
+    }
+
+}
+
+function swap(i, j, arr) {
+    arr[i], arr[j] = arr[j], arr[i]
+}
